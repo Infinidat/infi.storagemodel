eb9a5b6a6374cd33a8e6882c547b7cfb80e9fc07
Guy Rozendorn <guy@rzn.co.il>
2011-07-29 02:20:53 +0300

    changes to linux implementation
    moved one finder method here since its os specific
    fixed a typo

--- src/infi/storagemodel/linux/__init__.py
+++ src/infi/storagemodel/linux/__init__.py
@@ -12,14 +12,21 @@ class LinuxSCSIStorageController(base.SCSIStorageController, LinuxSCSIDevice):
     pass
 
 class LinuxScsiModel(base.ScsiModel):
-    pass
+    def find_scsi_block_device_by_devno(self, devno):
+        """ raises KeyError if not found. devno is type of str.
+        on linux: by major/minor
+        on windows: by number of physicaldrive
+        """
+        # TODO this is not cross-platform, should it be in here?
+        raise NotImplementedError
 
 class LinuxNativeMultipathModel(base.NativeMultipathModel):
     pass
 
-class LinuxStorageModel(StorageModel):
+class LinuxStorageModel(base.StorageModel):
     def _create_scsi_model(self):
         return LinuxScsiModel()
 
     def _create_native_multipath_model(self):
         return LinuxNativeMultipathModel()
+
