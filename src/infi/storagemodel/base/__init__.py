78f93d40c7dd031e3604cca378f225af3460ea42
Guy Rozendorn <guy@rzn.co.il>
2011-07-31 11:17:33 +0300

    multipath model update

--- src/infi/storagemodel/base/__init__.py
+++ src/infi/storagemodel/base/__init__.py
@@ -3,8 +3,11 @@ from ..utils import cached_property, cached_method, clear_cache, LazyImmutableDi
 from contextlib import contextmanager
 
 from .inquiry import SupportedVPDPagesDict, InquiryInformationMixin
-from .scsi import SCSIDevice, SCSIBlockDevice, SCSIStorageController, ScsiModel
+from .scsi import SCSIDevice, SCSIBlockDevice, SCSIModel, SCSIStorageController
 from .multipath import MultipathDevice, Path, MultipathFrameworkModel, NativeMultipathModel
+from .multipath import LoadBalancePolicy, LoadBalancingContext
+from .multipath import FailoverOnly, RoundRobin, RoundRobinWithExplicitSubset, RoundRobinWithTPGSSubset
+from .multipath import WeightedPaths, LeastBlocks, LeastQueueDepth
 
 class StorageModel(object):
     def __init__(self):
--- src/infi/storagemodel/base/multipath.py
+++ src/infi/storagemodel/base/multipath.py
@@ -6,8 +6,7 @@ from .inquiry import InquiryInformationMixin
 
 class MultipathFrameworkModel(object):
     def filter_non_multipath_scsi_block_devices(self, scsi_block_devices):
-        """ returns items from the list that are not part of multipath devices claimed by this framework
-        """
+        """ returns items from the list that are not part of multipath devices claimed by this framework"""
         hctl_list = [path.hctl for path in [multipath.paths for multipath in self.get_all_multipath_devices()]]
         filter (lambda device: device.hctl in hctl_list, scsi_block_devices)
 
@@ -21,16 +20,24 @@ class MultipathFrameworkModel(object):
 
     @cached_method
     def get_all_multipath_devices(self):
-        """ returns all multipath devices claimed by this framework
-        """
+        """ returns all multipath devices claimed by this framework"""
         # platform implementation
         raise NotImplementedError
-
 
 class NativeMultipathModel(MultipathFrameworkModel):
     pass
 
 class MultipathDevice(InquiryInformationMixin, object):
+    @cached_property
+    def vendor(self):
+        """Returns a vendor-specific implementation from the factory based on the device's SCSI vid and pid"""
+        from ..vendor import VendorFactory
+        return VendorFactory.create_multipath_by_vid_pid(self.scsi_vid_pid, self)
+
+    #############################
+    # Platform Specific Methods #
+    #############################
+
     @contextmanager
     def asi_context(self):
         # platform implementation
@@ -38,17 +45,11 @@ class MultipathDevice(InquiryInformationMixin, object):
 
     @cached_property
     def device_access_path(self):
-        """ linux: /dev/dm-X
-        windows: mpiodisk%d
-        """
         # platform implementation
         raise NotImplementedError
 
     @cached_property
     def display_name(self):
-        """ linux: mpathX
-        windows: physicaldrive
-        """
         # platform implementation
         raise NotImplementedError
 
@@ -64,49 +65,81 @@ class MultipathDevice(InquiryInformationMixin, object):
 
     @cached_property
     def policy(self):
-        """ 'failover only', 'round robin', 'weighted round robin', 'least queue depth', 'least blocks',
-        'round robin with subset'
-        not all policies are supported on all platforms
-        """
-        # return a Policy object (FailOverOnly/Custom/...)
-        # platform implementation
+        self._platform_specific_policy_context.get_policy_for_device(self)
+
+    def set_new_policy(self, policy):
+        # we're not going to do this now
         raise NotImplementedError
 
     @cached_property
-    def policy_attributes(self):
-        """ names of path attributes relevant to this policy
-        """
-        # platform implementation
+    def _platform_specific_policy_context(self):
         raise NotImplementedError
 
-    def apply_policy(self, policy_builder):
-        """
-        linux: 
-            failover only: group per path
-            round-robin: all paths in one group, we ignore path states, weights
-            weighted-paths: all paths in one group, allow weights
-            round-robin with subset: not supported
-            least queue depth: all paths in one group, selector is queue-length, not supported on all all distros
-            least blocks: all paths in hour group, select is service-time, not supported on all distros
-        windows: 
-            round-robin with subset: not supported
-            on invalid policy, ValueError is raised
-            """
-        # platform implementation
+class LoadBalancingContext(object):
+    def get_policy_for_device(self, device):
         raise NotImplementedError
+
+    def apply_policy_on_device(self, device, policy):
+        policy.apply_on_device(device)
+
+class LoadBalancePolicy(object):
+    _name = None
+    def __init__(self):
+        self._cache = dict()
 
     @cached_property
-    def vendor(self):
-        """ Returns a vendor-specific implementation from the factory based on the device's SCSI vid and pid"""
-        from ..vendor import VendorFactory
-        return VendorFactory.create_multipath_by_vid_pid(self.scsi_vid_pid, self)
+    def display_name(self):
+        return self._name
 
-class FailoverOnlyBuilder(object):
+    def apply_on_device(self, device):
+        raise NotImplementedError
+
+class FailoverOnly(LoadBalancePolicy):
+    _name = "FailOverOnly"
+
+    def __init__(self, active_path_id):
+        super(FailoverOnly, self).__init__()
+        self._cache["active_path_id"] = active_path_id
+
+    @cached_property
+    def active_path_id(self):
+        pass
+
+class RoundRobin(LoadBalancePolicy):
+    _name = "Round Robin"
+
+class RoundRobinWithSubset(LoadBalancePolicy):
+    _name = "Round Robin with subset"
+
+    def __init__(self, active_path_ids):
+        super(RoundRobinWithSubset, self).__init__()
+        self._cache["active_path_ids"] = active_path_ids
+
+    @property
+    def active_path_ids(self):
+        pass
+
+class RoundRobinWithTPGSSubset(RoundRobinWithSubset):
     pass
 
-class RoundRobinWithSubsetBuilder(object):
-    def use_tpgs(self):
-        return self
+class RoundRobinWithExplicitSubset(RoundRobinWithSubset):
+    pass
+
+class WeightedPaths(LoadBalancePolicy):
+    _name = "Weighted Paths"
+    def __init__(self, weights):
+        super(WeightedPaths, self).__init__()
+        self._cache['weights'] = weights
+
+    @property
+    def weights(self):
+        pass
+
+class LeastBlocks(LoadBalancePolicy):
+    _name = "Least Blocks"
+
+class LeastQueueDepth(LoadBalancePolicy):
+    _name = "Least Queue Depth"
 
 class Path(object):
     @cached_property
@@ -135,6 +168,3 @@ class Path(object):
         """up/down"""
         # platform implementation
         raise NotImplementedError
-
-
-# TODO the policy strategy
