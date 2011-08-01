2f1ead7b2f19d70eaadd062f6d487c75ef08d7f9
Guy Rozendorn <guy@rzn.co.il>
2011-08-02 01:50:44 +0300

    fixes linux imports during refactoring
    Yalon, if this commit gives you merge errors, ignore it

--- src/infi/storagemodel/linux/__init__.py
+++ src/infi/storagemodel/linux/__init__.py
@@ -1,17 +1,17 @@
-from .. import base
+from ..base import StorageModel, scsi, multipath
 
 class LinuxSCSIDevice(object):
     @property
     def unix_devno(self):
         pass
 
-class LinuxSCSIBlockDevice(base.SCSIBlockDevice, LinuxSCSIDevice):
+class LinuxSCSIBlockDevice(scsi.SCSIBlockDevice, LinuxSCSIDevice):
     pass
 
-class LinuxSCSIStorageController(base.SCSIStorageController, LinuxSCSIDevice):
+class LinuxSCSIStorageController(scsi.SCSIStorageController, LinuxSCSIDevice):
     pass
 
-class LinuxScsiModel(base.SCSIModel):
+class LinuxScsiModel(scsi.SCSIModel):
     def find_scsi_block_device_by_devno(self, devno):
         """ raises KeyError if not found. devno is type of str.
         on linux: by major/minor
@@ -20,10 +20,10 @@ class LinuxScsiModel(base.SCSIModel):
         # TODO this is not cross-platform, should it be in here?
         raise NotImplementedError
 
-class LinuxNativeMultipathModel(base.NativeMultipathModel):
+class LinuxNativeMultipathModel(multipath.NativeMultipathModel):
     pass
 
-class LinuxStorageModel(base.StorageModel):
+class LinuxStorageModel(StorageModel):
     def _create_scsi_model(self):
         return LinuxScsiModel()
 
