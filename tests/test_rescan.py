c3b6314d464c97f1ed20c96035c4154550b9d5df
Guy Rozendorn <guy@rzn.co.il>
2011-08-09 21:03:36 +0300

    fixing tests

--- tests/test_rescan.py
+++ tests/test_rescan.py
@@ -14,7 +14,7 @@ class TestModel(unittest.TestCase):
 
     def test_rescan__timeout(self):
         from infi.storagemodel import get_storage_model
-        from infi.storagemodel.base import TimeoutError
+        from infi.storagemodel.errors import TimeoutError
         from infi.storagemodel.predicates import DiskExists
         self.assertRaises(TimeoutError, get_storage_model().rescan_and_wait_for, *(DiskExists("fooBar"), 1))
 
