99646264fea9094be74f818803e88ab46d49055d
Guy Rozendorn <guy@rzn.co.il>
2012-03-12 23:21:45 +0200

    TRIVIAL safer __repr__s

--- src/infi/storagemodel/vendor/infinidat/infinibox/mixin/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/infinibox/mixin/__init__.py
@@ -11,17 +11,13 @@ from .sophisticated import SophisticatedMixin
 
 class block_class(InfiniBoxInquiryMixin, SophisticatedMixin, InfiniBoxVolumeMixin, VendorSCSIBlockDevice):
     def __repr__(self):
-        msg = "<Block device for volume {} of system {}, mapped to host {}, address {}>"
-        return msg.format(self.get_volume_id(), self.get_system_serial(),
-                          self.get_host_id(), self.get_management_address())
+        return "<Infinibox Mixin for {!r}>".format(self.device)
 
 class controller_class(InfiniBoxInquiryMixin, SophisticatedMixin, VendorSCSIStorageController):
     def __repr__(self):
-        msg = "<Controller for system {}, mapped to host {}, address {} >"
-        return msg.format(self.get_system_serial(), self.get_host_id(), self.get_management_address())
+        return "<Infinibox Mixin for {!r}>".format(self.device)
 
 class multipath_class(InfiniBoxInquiryMixin, SophisticatedMixin, InfiniBoxVolumeMixin, VendorMultipathDevice):
     def __repr__(self):
-        msg = "<Multipath device for volume {} of system {}, mapped to host {}, address {}>"
-        return msg.format(self.get_volume_id(), self.get_system_serial(),
-                          self.get_host_id(), self.get_management_address())
+        return "<Infinibox Mixin for {!r}>".format(self.device)
+
