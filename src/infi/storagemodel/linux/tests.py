4963ecaab6211fd124f25f1d75be1b23e9bf4c07
Guy Rozendorn <guy@rzn.co.il>
2012-04-30 08:37:27 +0300

    STORAGEMODEL-119 changing rescan mechanism on linux
    This reverts commit 6e84df636db814a118cc19c464f2ffbfb49f6ec9.

--- src/infi/storagemodel/linux/tests.py
+++ src/infi/storagemodel/linux/tests.py
@@ -9,23 +9,5 @@ from . import LinuxStorageModel
 class InitiateRescan(unittest.TestCase):
 
     def test_for_real(self):
-        from . import is_rescan_script_exists
-        if not is_rescan_script_exists():
-            raise unittest.SkipTest
         model = LinuxStorageModel()
         model.initiate_rescan()
-
-    def test__no_script_found(self):
-        from ..errors import StorageModelError
-        from .. import linux
-
-        original_call_function = linux._call_rescan_script
-
-        def side_effect(*args, **kwargs):
-            raise StorageModelError
-
-        with mock.patch("infi.storagemodel.linux._call_rescan_script") as patch:
-            model = LinuxStorageModel()
-            patch.side_effect = side_effect
-            self.assertRaises(StorageModelError, model.initiate_rescan)
-            self.assertTrue(patch.called)
