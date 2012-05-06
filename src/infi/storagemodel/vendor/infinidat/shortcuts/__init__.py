10ca31c2a44172cc16cb3005bd972c32ced273a3
Guy Rozendorn <guy@rzn.co.il>
2012-05-06 11:02:10 +0300

    TRIVIAL making infi.storagemodel.vendor.infinidat.shortcuts patching friendly

--- src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
@@ -1,22 +1,22 @@
 
-from infi.storagemodel import get_storage_model
+import infi.storagemodel
 from ..infinibox import vid_pid
 
 def get_infinidat_storage_controller_devices():
-    model = get_storage_model().get_scsi()
+    model = infi.storagemodel.get_storage_model().get_scsi()
     return model.filter_vendor_specific_devices(model.get_all_storage_controller_devices(), vid_pid)
 
 def get_infinidat_scsi_block_devices():
-    model = get_storage_model().get_scsi()
+    model = infi.storagemodel.get_storage_model().get_scsi()
     return model.filter_vendor_specific_devices(model.get_all_scsi_block_devices(), vid_pid)
 
 def get_infinidat_native_multipath_devices():
-    model = get_storage_model().get_native_multipath()
+    model = infi.storagemodel.get_storage_model().get_native_multipath()
     return model.filter_vendor_specific_devices(model.get_all_multipath_devices(), vid_pid)
 
 def get_infinidat_non_multiathed_scsi_block_devices():
     all_scsi = get_infinidat_scsi_block_devices()
-    model = get_storage_model().get_native_multipath()
+    model = infi.storagemodel.get_storage_model().get_native_multipath()
     return model.filter_non_multipath_scsi_block_devices(all_scsi)
 
 def get_infinidat_block_devices():
