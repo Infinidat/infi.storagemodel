33fa1e29c901abec7827925b8bddd5d33eb72a4e
Arnon Yaari <wiggin15@yahoo.com>
2013-01-01 12:48:20 +0200

    STORAGEMODEL-199 implement string representation for InfinidatNAA

--- src/infi/storagemodel/vendor/infinidat/infinibox/naa/tests.py
+++ src/infi/storagemodel/vendor/infinidat/infinibox/naa/tests.py
@@ -2,9 +2,12 @@ from unittest import TestCase
 from . import InfinidatNAA
 from .. import NFINIDAT_IEEE
 
+TEST_NAA = "6742b0f000004e2b000000000000018c"
+
 class InfinidatNAATest(TestCase):
     def test_string_param(self):
-        naa = InfinidatNAA("6742b0f000004e2b000000000000018c".decode("hex"))
+        naa = InfinidatNAA(TEST_NAA.decode("hex"))
         self.assertEqual(naa.get_volume_id(), 0x18c)
         self.assertEqual(naa.get_system_serial(), 0x4e2b)
         self.assertEqual(naa.get_ieee_company_id(), NFINIDAT_IEEE)
+        self.assertEqual(str(naa), "naa." + TEST_NAA)
