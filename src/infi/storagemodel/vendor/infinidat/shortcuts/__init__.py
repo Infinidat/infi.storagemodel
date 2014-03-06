c526df866b057ae631774c7e5a23a14bb0145ba7
Guy Rozendorn <guy@rzn.co.il>
2014-03-06 19:01:07 +0200

    STORAGEMODEL-256 fixing typo

--- src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
@@ -18,21 +18,21 @@ def get_infinidat_native_multipath_storage_controller_devices():
     model = infi.storagemodel.get_storage_model().get_native_multipath()
     return model.filter_vendor_specific_devices(model.get_all_multipath_storage_controller_devices(), vid_pid)
 
-def get_infinidat_non_multiathed_scsi_block_devices():
+def get_infinidat_non_multipathed_scsi_block_devices():
     all_scsi = get_infinidat_scsi_block_devices()
     model = infi.storagemodel.get_storage_model().get_native_multipath()
     return model.filter_non_multipath_scsi_block_devices(all_scsi)
 
-def get_infinidat_non_multiathed_scsi_storage_controller_devices():
+def get_infinidat_non_multipathed_scsi_storage_controller_devices():
     all_scsi = get_infinidat_scsi_storage_controller_devices()
     model = infi.storagemodel.get_storage_model().get_native_multipath()
     return model.filter_non_multipath_scsi_storage_controller_devices(all_scsi)
 
 def get_infinidat_block_devices():
-    return get_infinidat_native_multipath_block_devices() + get_infinidat_non_multiathed_scsi_block_devices()
+    return get_infinidat_native_multipath_block_devices() + get_infinidat_non_multipathed_scsi_block_devices()
 
 def get_infinidat_storage_controller_devices():
-    return get_infinidat_native_multipath_storage_controller_devices() + get_infinidat_non_multiathed_scsi_storage_controller_devices()
+    return get_infinidat_native_multipath_storage_controller_devices() + get_infinidat_non_multipathed_scsi_storage_controller_devices()
 
 def get_infinidat_scsi_devices():
     return get_infinidat_scsi_block_devices() + get_infinidat_scsi_storage_controller_devices()
