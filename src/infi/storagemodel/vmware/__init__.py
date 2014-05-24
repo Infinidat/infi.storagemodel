7db71172eea16dcf74ed42a7ecce5cd4a78cf823
Guy Rozendorn <guy@rzn.co.il>
2014-05-24 16:35:44 +0300

    VMWare support

---
+++ src/infi/storagemodel/vmware/__init__.py
@@ -0,0 +1,20 @@
+from infi.pyutils.contexts import contextmanager
+
+
+@contextmanager
+def with_host(host):
+    from .patches import storagemodel, hbaapi
+    with hbaapi.with_host(host), storagemodel.with_host(host):
+        yield
+
+
+def install_all_property_collectors_on_client(client):
+    from .patches import storagemodel, hbaapi
+    from pyvsdk import Vim
+    storagemodel.install_property_collectors_on_client(client)
+    hbaapi.install_property_collectors_on_client(client)
+
+
+def fetch_all_property_collectors(client):
+    for property_collector in client.facades.values():
+        property_collector.getProperties()
