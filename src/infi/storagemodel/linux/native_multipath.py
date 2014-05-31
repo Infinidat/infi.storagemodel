0b799fb72df6eb3af85a35fc9e832c834bc9c14d
Guy Rozendorn <guy@rzn.co.il>
2014-05-31 21:25:19 +0300

    STORAGEMODEL-275 increasing the timeout used in communicated with multipathd to 120 seconds

--- src/infi/storagemodel/linux/native_multipath.py
+++ src/infi/storagemodel/linux/native_multipath.py
@@ -124,8 +124,8 @@ class LinuxNativeMultipathModel(multipath.NativeMultipathModel):
 
     @cached_method
     def get_all_multipath_block_devices(self):
-        from infi.multipathtools import MultipathClient
-        client = MultipathClient()
+        from infi.multipathtools import MultipathClient, UnixDomainSocket
+        client = MultipathClient(UnixDomainSocket(timeout=120))
         if not client.is_running():
             logger.info("MultipathD is not running")
             return []
