6d61a6eae7a525b22e21d05eb15acd1502e87597
Guy Rozendorn <guy@rzn.co.il>
2014-06-20 13:44:31 +0300

    TRIVIAL InfiniBox vid_pid_with_spaces

--- src/infi/storagemodel/vendor/infinidat/infinibox/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/infinibox/__init__.py
@@ -4,5 +4,6 @@ log = getLogger(__name__)
 NFINIDAT_IEEE = 0x742B0F
 ALIGNMENT = 64*1024
 vid_pid = ("NFINIDAT", "InfiniBox")
+vid_pid_with_spaces = ("NFINIDAT".ljust(8), "InfiniBox".ljust(16))
 
 # pylint: disable=C0103
