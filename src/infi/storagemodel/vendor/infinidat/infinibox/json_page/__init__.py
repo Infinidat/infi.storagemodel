ad2abf670bec7142e1e616b71d89efb48c9e61d1
Guy Rozendorn <guy@rzn.co.il>
2014-08-19 00:01:59 +0300

    STORAGEMODEL-228 fixing JSONInquiryPageBuffer

--- src/infi/storagemodel/vendor/infinidat/infinibox/json_page/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/infinibox/json_page/__init__.py
@@ -1,12 +1,16 @@
 from infi.instruct.buffer import Buffer, buffer_field, bytes_ref, be_uint_field, len_ref, self_ref, json_field
 from infi.asi.cdb.inquiry import PeripheralDeviceDataBuffer
 from infi.asi.cdb.inquiry.vpd_pages import EVPDInquiryCommand
-
+import json
 
 class JSONInquiryPageBuffer(Buffer):
+    def _calc_json_length(self):
+        import json
+        return len(json.dumps(self.json_data))
+
     peripheral_device = buffer_field(where=bytes_ref[0:], type=PeripheralDeviceDataBuffer)
     page_code = be_uint_field(where=bytes_ref[1])
-    page_length = be_uint_field(where=bytes_ref[2:4], set_before_pack=len_ref(self_ref.json_data))
+    page_length = be_uint_field(where=bytes_ref[2:4], set_before_pack=self_ref._calc_json_length())
     json_data = json_field(where=bytes_ref[4:4+page_length], unpack_after=page_length)
 
 
