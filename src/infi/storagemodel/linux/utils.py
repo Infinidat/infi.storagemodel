e302a1486db667d2e8c5448e97fe62b2f8c4f764
Guy Rozendorn <guy@rzn.co.il>
2015-01-25 16:07:49 +0200

    code review

--- src/infi/storagemodel/linux/utils.py
+++ src/infi/storagemodel/linux/utils.py
@@ -5,3 +5,17 @@ class LinuxUtils(Utils):
         from os import statvfs
         stat_res = statvfs(path)
         return stat_res.f_frsize * stat_res.f_bavail
+
+
+def execute_command(cmd, check_returncode=True, timeout=WAIT_TIME):  # pragma: no cover
+    from infi.execute import execute
+    logger.info("executing {}".format(cmd))
+    process = execute(cmd)
+    process.wait(WAIT_TIME)
+    logger.info("execution returned {}".format(process.get_returncode()))
+    logger.debug("stdout: {}".format(process.get_stdout()))
+    logger.debug("stderr: {}".format(process.get_stderr()))
+    if check_returncode and process.get_returncode() != 0:
+        formatted_cmd = cmd if isinstance(cmd, basestring) else repr(' '.join(cmd))
+        raise RuntimeError("execution of {} failed".format(formatted_cmd))
+    return process
--- src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/shortcuts/__init__.py
@@ -29,7 +29,10 @@ def get_infinidat_non_multipathed_scsi_block_devices():
 
 def get_infinidat_non_veritas_multipathed_scsi_block_devices():
     all_scsi = get_infinidat_scsi_block_devices()
-    model = infi.storagemodel.get_storage_model().get_veritas_multipath()
+    try:
+        model = infi.storagemodel.get_storage_model().get_veritas_multipath()
+    except NotImplementedError:
+        return []
     return model.filter_non_multipath_scsi_block_devices(all_scsi)
 
 def get_infinidat_non_multipathed_scsi_storage_controller_devices():
@@ -38,6 +41,15 @@ def get_infinidat_non_multipathed_scsi_storage_controller_devices():
     return model.filter_non_multipath_scsi_storage_controller_devices(all_scsi)
 
 def get_infinidat_block_devices():
+    """returns a list on NFINIDAT devices
+
+    the devices returned by this function is by priority:
+    * if a veritas multipath device exist, return those
+    * else if native multipath devices exist, return those
+    * else return the single-path deivces
+    """
+    veritas_multipath_devices = get_infinidat_veritas_multipath_block_devices()
+    native_multipath_devices = get_infinidat_native_multipath_block_devices()
     return get_infinidat_native_multipath_block_devices() + get_infinidat_non_multipathed_scsi_block_devices() + \
            get_infinidat_veritas_multipath_block_devices()
 
