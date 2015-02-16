610b2db6330b5648c2c35256802d301f22ec7ef0
Shai Keren <shaik@infinidat.com>
2015-02-16 17:35:57 +0200

    TRIVIAL fixed test_veritas_multipathing

--- tests/test_veritas_multipathing.py
+++ tests/test_veritas_multipathing.py
@@ -1,3 +1,5 @@
+from infi.storagemodel.linux.scsi import LinuxSCSIBlockDevice
+from infi.dtypes.hctl import HCTL
 from infi.storagemodel import get_storage_model
 from infi.pyutils.contexts import contextmanager
 from infi.pyutils.lazy import clear_cache
@@ -72,67 +74,72 @@ path        = sdy enabled(a) - FC c3 c3 2-2 57:42:b0:f0:00:4e:4f:12 -
 path        = sdaj disabled(a) - FC c3 c3 1-1 57:42:b0:f0:00:4e:4f:31 -
 """
 
+
+class MockSCSIBlockDevice(LinuxSCSIBlockDevice):
+    def get_hctl(self):
+        return HCTL(1,2,3,4)
+
+
 @contextmanager
-def veritas_multipathing_context(read_paths_list_func, output):
+def veritas_multipathing_context(output):
     if name == "nt":
         raise SkipTest
-    read_paths_list_func.return_value = output
-    sm = get_storage_model()
-    clear_cache(sm)
-    yield sm.get_veritas_multipath()
+    with patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list') as read_path_list:
+        with patch('infi.storagemodel.linux.sysfs.Sysfs.find_scsi_disk_by_hctl') as find_scsi_disk_by_hctl:
+            with patch('infi.storagemodel.base.scsi.SCSIModel.find_scsi_block_device_by_block_access_path') as find_func:
+                find_func.return_value = MockSCSIBlockDevice(None)
+                find_scsi_disk_by_hctl.return_value = None
+                read_path_list.return_value = output
+                sm = get_storage_model()
+                clear_cache(sm)
+                yield sm.get_veritas_multipath()
 
 
 class VeritasMultipathingTestCase(TestCase):
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output(self, read_paths_list):
+    def test_vxdmpadm_output(self):
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths=SIX_PATHS, vid="NFINIDAT", pid="InfiniBox")
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = veritas_multipath.get_all_multipath_block_devices()
             self.assertEquals(len(block_devices), 1)
             self.assertEquals(len(block_devices[0].get_paths()), 6)
 
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output_no_paths(self, read_paths_list):
+    def test_vxdmpadm_output_no_paths(self):
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths="", vid="NFINIDAT", pid="InfiniBox")
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = veritas_multipath.get_all_multipath_block_devices()
             self.assertEquals(len(block_devices), 0)
 
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output_all_paths_disabled(self, read_paths_list):
+    def test_vxdmpadm_output_all_paths_disabled(self):
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths=ALL_DISABLED, vid="NFINIDAT", pid="InfiniBox")
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = veritas_multipath.get_all_multipath_block_devices()
             self.assertEquals(len(block_devices), 0)
 
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output_one_path_enabled(self, read_paths_list):
+    def test_vxdmpadm_output_one_path_enabled(self):
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths=ONE_ENABLED, vid="NFINIDAT", pid="InfiniBox")
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = veritas_multipath.get_all_multipath_block_devices()
             self.assertEquals(len(block_devices), 1)
             self.assertEquals(len(block_devices[0].get_paths()), 6)
             self.assertEquals(len([p for p in block_devices[0].get_paths() if p.get_state() == "up"]), 1)
 
     @patch('infi.storagemodel.base.inquiry.InquiryInformationMixin.get_scsi_vendor_id_or_unknown_on_error')
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output_bad_vid(self, read_paths_list, get_scsi_vendor_id_or_unknown_on_error):
+    def test_vxdmpadm_output_bad_vid(self, get_scsi_vendor_id_or_unknown_on_error):
         from infi.storagemodel.vendor.infinidat.shortcuts import get_infinidat_veritas_multipath_block_devices
         vid, pid = ("NFINIDAS", "InfiniBox")
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths=SIX_PATHS, vid=vid, pid=pid)
         get_scsi_vendor_id_or_unknown_on_error.return_value = vid, pid
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = get_infinidat_veritas_multipath_block_devices()
             self.assertEquals(len(block_devices), 0)
 
     @patch('infi.storagemodel.base.inquiry.InquiryInformationMixin.get_scsi_vendor_id_or_unknown_on_error')
-    @patch('infi.storagemodel.linux.veritas_multipath.VeritasMultipathClient.read_paths_list')
-    def test_vxdmpadm_output_bad_pid(self, read_paths_list, get_scsi_vendor_id_or_unknown_on_error):
+    def test_vxdmpadm_output_bad_pid(self, get_scsi_vendor_id_or_unknown_on_error):
         from infi.storagemodel.vendor.infinidat.shortcuts import get_infinidat_veritas_multipath_block_devices
         vid, pid = ("NFINIDAT", "InfiniBos")
         vxdmpadm_output = VXDMPADM_OUTPUT_TEMPLATE.format(paths=SIX_PATHS, vid=vid, pid=pid)
         get_scsi_vendor_id_or_unknown_on_error.return_value = (vid, pid)
-        with veritas_multipathing_context(read_paths_list, vxdmpadm_output) as veritas_multipath:
+        with veritas_multipathing_context(vxdmpadm_output) as veritas_multipath:
             block_devices = get_infinidat_veritas_multipath_block_devices()
             self.assertEquals(len(block_devices), 0)
 
