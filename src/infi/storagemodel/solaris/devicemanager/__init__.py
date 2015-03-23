2e398089bf3c7ea8805e10c04e805deb1bd1d5d1
Shai Keren <shaik@infinidat.com>
2015-03-23 19:45:49 +0200

    HOSTDEV-1810 implemented kstat io stats

--- src/infi/storagemodel/solaris/devicemanager/__init__.py
+++ src/infi/storagemodel/solaris/devicemanager/__init__.py
@@ -10,51 +10,51 @@ MULTIPATH_DEVICE_PATH = "/dev/scsi/array_ctrl"
 DEVICE_MAP_PATH = "/etc/path_to_inst"
 
 class SolarisBlockDevice(object):
-	def __init__(self, controller, target, disk, d_slice=''):
-		self._controller = controller
-		self._target = target
-		self._disk = disk
-		self._slice = d_slice
+    def __init__(self, controller, target, disk, d_slice=''):
+        self._controller = controller
+        self._target = target
+        self._disk = disk
+        self._slice = d_slice
 
-	def _get_device_map(self):
-		device_map_data = open(DEVICE_MAP_PATH, "rb").read()
-		devices = findall("\"(.*)\" (\d*) \"(.*)\"", device_map_data)
-		device_map = {}
-		for device in devices:
-			device_map[device[0]] = "{}{}".format(device[2], device[1])
-		return device_map
+    def _get_device_map(self):
+        device_map_data = open(DEVICE_MAP_PATH, "rb").read()
+        devices = findall("\"(.*)\" (\d*) \"(.*)\"", device_map_data)
+        device_map = {}
+        for device in devices:
+            device_map[device[0]] = "{}{}".format(device[2], device[1])
+        return device_map
 
-	def get_controller(self):
-		return self._controller
+    def get_controller(self):
+        return self._controller
 
-	def get_target(self):
-		return self._target
+    def get_target(self):
+        return self._target
 
-	def get_disk(self):
-		return self._disk
+    def get_disk(self):
+        return self._disk
 
-	def get_device_name(self):
-		slice_string = "s{}".format(self._slice) if not self._slice == '' else ''
-		return "c{}t{}d{}{}".format(self._controller, self._target, self._disk, slice_string)
+    def get_device_name(self):
+        slice_string = "s{}".format(self._slice) if not self._slice == '' else ''
+        return "c{}t{}d{}{}".format(self._controller, self._target, self._disk, slice_string)
 
-	@cached_method
-	def get_sd_name(self):
-		# return device name in a format like sd1,a
-		from os import readlink, path
-		device_path = readlink(path.join(DISK_DEVICE_PATH, self.get_device_name())).replace("../../devices", "")
-		device_sd_name = self._get_device_map()[device_path.split(":")[0]]
-		device_slice = device_path.split(":")[1]
-		return "{},{}".format(device_sd_name, device_slice)
+    @cached_method
+    def get_sd_name(self):
+        # return device name in a format like sd1,a
+        from os import readlink, path
+        device_path = readlink(path.join(DISK_DEVICE_PATH, self.get_device_name())).replace("../../devices", "")
+        device_sd_name = self._get_device_map()[device_path.split(":")[0]]
+        device_slice = device_path.split(":")[1]
+        return "{},{}".format(device_sd_name, device_slice)
 
-	def __str__(self):
-		return self.get_device_name()
+    def __str__(self):
+        return self.get_device_name()
 
-	def get_device_path(self):
-		from os.path import join
-		return join([DISK_DEVICE_PATH, self.get_device_name()])
+    def get_device_path(self):
+        from os.path import join
+        return join([DISK_DEVICE_PATH, self.get_device_name()])
 
     def get_vendor(self):
-    	# need to use the kstat library to get this info offline
+        # need to use the kstat library to get this info offline
         raise NotImplemented()
 
     def get_model(self):
@@ -64,29 +64,29 @@ class SolarisBlockDevice(object):
         raise NotImplemented()
 
     def get_size_in_bytes(self):
-    	raise NotImplemented()
+        raise NotImplemented()
 
 class DeviceManager(object):
-	def __init__(self):
-		device_list = []
+    def __init__(self):
+        device_list = []
 
-	def _get_device_object(self, dev_string):
-		try:
-			device_info = findall("c(.*)t(.*)d(\d*)(?:s(\d*))?", dev_string)[0]
-			return SolarisBlockDevice(*device_info)
-		except:
-			logger.exception("Can't parse device name: {}".format(dev_string))
-			return None
+    def _get_device_object(self, dev_string):
+        try:
+            device_info = findall("c(.*)t(.*)d(\d*)(?:s(\d*))?", dev_string)[0]
+            return SolarisBlockDevice(*device_info)
+        except:
+            logger.exception("Can't parse device name: {}".format(dev_string))
+            return None
 
-	def get_all_devices(self):
-		devlist = []
-		from os import listdir
-		from os.path import exists, join
-		for device in listdir(DISK_DEVICE_PATH):
-			if not exists(join(DISK_DEVICE_PATH, device)):
-				continue
-			if device.endswith("d0"):
-				devlist.append(device)
-			elif device.endswith("s2") and device[:-2] not in devlist:
-				devlist.append(device)
-		return [self._get_device_object(device) for device in devlist]
+    def get_all_devices(self):
+        devlist = []
+        from os import listdir
+        from os.path import exists, join
+        for device in listdir(DISK_DEVICE_PATH):
+            if not exists(join(DISK_DEVICE_PATH, device)):
+                continue
+            if device.endswith("d0"):
+                devlist.append(device)
+            elif device.endswith("s2") and device[:-2] not in devlist:
+                devlist.append(device)
+        return [self._get_device_object(device) for device in devlist]
--- src/infi/storagemodel/solaris/devicemanager/kstat.py
+++ src/infi/storagemodel/solaris/devicemanager/kstat.py
@@ -1,78 +1,136 @@
-from ctypes import *
+import sys
+import ctypes
+from infi.pyutils.contexts import contextmanager
+from infi.pyutils.lazy import cached_method
+from infi.storagemodel.base.multipath import PathStatistics
+from infi.cwrap import WrappedFunction, errcheck_nothing, IN, IN_OUT
+
 from logging import getLogger
-
 logger = getLogger(__name__)
-
-"""
-from /usr/include/sys/kstat.h
-
-#define	KSTAT_IOC_BASE		('K' << 8)
-#define	KSTAT_IOC_CHAIN_ID	KSTAT_IOC_BASE | 0x01
-#define	KSTAT_IOC_READ		KSTAT_IOC_BASE | 0x02
-#define	KSTAT_IOC_WRITE		KSTAT_IOC_BASE | 0x03
-
-#define	KSTAT_STRLEN	31	/* 30 chars + NULL; must be 16 * n - 1 */
-
-/* The generic kstat header */
-
-typedef struct kstat {
-	/*
-	 * Fields relevant to both kernel and user
-	 */
-	hrtime_t	ks_crtime;	/* creation time (from gethrtime()) */
-	struct kstat	*ks_next;	/* kstat chain linkage */
-	kid_t		ks_kid;		/* unique kstat ID */
-	char		ks_module[KSTAT_STRLEN]; /* provider module name */
-	uchar_t		ks_resv;	/* reserved, currently just padding */
-	int		ks_instance;	/* provider module's instance */
-	char		ks_name[KSTAT_STRLEN]; /* kstat name */
-	uchar_t		ks_type;	/* kstat data type */
-	char		ks_class[KSTAT_STRLEN]; /* kstat class */
-	uchar_t		ks_flags;	/* kstat flags */
-	void		*ks_data;	/* kstat type-specific data */
-	uint_t		ks_ndata;	/* # of type-specific data records */
-	size_t		ks_data_size;	/* total size of kstat data section */
-	hrtime_t	ks_snaptime;	/* time of last data shapshot */
-	/*
-	 * Fields relevant to kernel only
-	 */
-	int		(*ks_update)(struct kstat *, int); /* dynamic update */
-	void		*ks_private;	/* arbitrary provider-private data */
-	int		(*ks_snapshot)(struct kstat *, void *, int);
-	void		*ks_lock;	/* protects this kstat's data */
-} kstat_t;
-
-
-"""
 
 KSTAT_STRLEN = 31
 
-c_kid_t = c_int
-c_kstat_string = c_char * KSTAT_STRLEN
-c_hrtime_t = c_longlong
+KSTAT_TYPE_RAW       =  0
+KSTAT_TYPE_NAMED     =  1
+KSTAT_TYPE_INTR      =  2
+KSTAT_TYPE_IO        =  3
+KSTAT_TYPE_TIMER     =  4
+KSTAT_NUM_TYPES      =  5
 
-class _kstat_t(Structure):
+c_kid_t = ctypes.c_int
+c_kstat_string = ctypes.c_char * KSTAT_STRLEN
+c_hrtime_t = ctypes.c_longlong
+
+class kstat_t(ctypes.Structure):
     pass
-
-_kstat_t_p = POINTER(_kstat_t)
-
-_kstat_t._fields_ = [
-    ('ks_crtime', c_hrtime_t),
-    ('ks_next', _kstat_t_p),
-    ('ks_kid', c_kid_t),
-    ('ks_module', c_kstat_string),
-    ('ks_resv', c_ubyte),
-    ('ks_instance', c_int),
-    ('ks_name', c_kstat_string),
-    ('ks_type', c_ubyte),
-    ('ks_class', c_kstat_string),
-    ('ks_flags', c_ubyte),
-    ('ks_data', c_void_p),
-    ('ks_ndata', c_uint),
-    ('ks_data_size', c_size_t),
-    ('ks_snaptime', c_hrtime_t),
-    ('ks_update', c_void_p),
-    ('ks_private', c_void_p),
-    ('ks_snapshot', c_void_p),
-    ('ks_lock', c_void_p)
+kstat_t_p = ctypes.POINTER(kstat_t)
+kstat_t._fields_ = [
+        ('ks_crtime', c_hrtime_t),
+        ('ks_next', kstat_t_p),
+        ('ks_kid', c_kid_t),
+        ('ks_module', c_kstat_string),
+        ('ks_resv', ctypes.c_ubyte),
+        ('ks_instance', ctypes.c_int),
+        ('ks_name', c_kstat_string),
+        ('ks_type', ctypes.c_ubyte),
+        ('ks_class', c_kstat_string),
+        ('ks_flags', ctypes.c_ubyte),
+        ('ks_data', ctypes.c_void_p),
+        ('ks_ndata', ctypes.c_uint),
+        ('ks_data_size', ctypes.c_size_t),
+        ('ks_snaptime', c_hrtime_t),
+        ('ks_update', ctypes.c_void_p),
+        ('ks_private', ctypes.c_void_p),
+        ('ks_snapshot', ctypes.c_void_p),
+        ('ks_lock', ctypes.c_void_p)
 ]
+
+class kstat_ctl_t(ctypes.Structure):
+    _fields_ = [
+        ('kc_chain_id', c_kid_t),
+        ('kc_chain', kstat_t_p),
+        ('kc_kd', ctypes.c_int)
+    ]
+kstat_ctl_t_p = ctypes.POINTER(kstat_ctl_t)
+
+class kstat_io_t(ctypes.Structure):
+    _fields_ = [
+        ('nread',ctypes.c_ulonglong),
+        ('nwritten', ctypes.c_ulonglong),
+        ('reads',ctypes.c_uint),
+        ('writes',ctypes.c_uint),
+        ('wtime',c_hrtime_t),
+        ('wlentime',c_hrtime_t),
+        ('wlastupdate',c_hrtime_t),
+        ('rtime',c_hrtime_t),
+        ('rlentime',c_hrtime_t),
+        ('rlastupdate',c_hrtime_t),
+        ('wcnt',ctypes.c_uint),
+        ('rcnt',ctypes.c_uint),
+    ]
+kstat_io_t_p = ctypes.POINTER(kstat_io_t)
+
+
+class KStatFunction(WrappedFunction):
+    @classmethod
+    def get_library_name(cls):
+        return '/lib/amd64/libkstat.so' if sys.maxsize > 2 ** 32 else '/lib/libkstat.so'
+
+    @classmethod
+    def get_errcheck(cls):
+        return errcheck_nothing()
+
+    @classmethod
+    def _get_library(cls):
+        return ctypes.cdll.LoadLibrary(cls.get_library_name())
+
+
+class kstat_open(KStatFunction):
+    return_value = kstat_ctl_t_p
+
+    @classmethod
+    def get_parameters(cls):
+        return ()
+
+class kstat_close(KStatFunction):
+    return_value = ctypes.c_int
+
+    @classmethod
+    def get_parameters(cls):
+        return (kstat_ctl_t_p)
+
+class kstat_read(KStatFunction):
+    return_value = c_kid_t
+
+    @classmethod
+    def get_parameters(cls):
+        return ((kstat_ctl_t_p, IN),
+                (kstat_t_p, IN),
+                (kstat_io_t_p, IN_OUT))
+
+@contextmanager
+def kstat_context():
+    ks = kstat_open()
+    try:
+        yield ks
+    except:
+        kstat_close(ks)
+
+class KStat(object):
+    def _get_path_statistics(self, ks, kc):
+        iostats = kstat_io_t()
+        kstat_read(ks, kc, ctypes.byref(iostats)) # TODO Check return value
+        return PathStatistics(iostats.nread, iostats.nwritten, iostats.reads, iostats.writes)
+
+    @cached_method
+    def get_io_stats(self):
+        res = {}
+        with kstat_context() as ks:
+            elem = ks.contents.kc_chain.contents
+            while True:
+                if elem and elem.ks_type == KSTAT_TYPE_IO:
+                    res[elem.ks_name] = self._get_path_statistics(ks, elem)
+                if not elem.ks_next:
+                    break
+                elem = elem.ks_next.contents
+        return res
