732ae74edad93793c833206dfab0bb46528feea6
Arnon Yaari <arnony@infinidat.com>
2016-02-21 16:47:04 +0200

    HPTVM-951 add predicate for non-zero cluster id

--- src/infi/storagemodel/vendor/infinidat/predicates/__init__.py
+++ src/infi/storagemodel/vendor/infinidat/predicates/__init__.py
@@ -61,7 +61,7 @@ class FiberChannelMappingNotExistsUsingLinuxSG(FiberChannelMappingExistsUsingLin
         return not super(FiberChannelMappingNotExistsUsingLinuxSG, self).__call__()
 
 
-def get_predicate_for_checking_non_zero_host_id(system_serial):
+def get_predicate_for_checking_non_zero_host_id(system_serial, cluster_id=False):
     def all_storage_devices_on_logical_unit_0_of_specific_box_show_non_zero_host_id():
         from infi.storagemodel.vendor.infinidat.shortcuts import get_infinidat_block_devices_and_controllers__mapped_to_lun0
         from infi.storagemodel.errors import RescanIsNeeded
@@ -73,8 +73,12 @@ def get_predicate_for_checking_non_zero_host_id(system_serial):
             pass
 
         for device in devices:
-            if device.get_vendor().get_system_serial() == system_serial and device.get_vendor().get_host_id() == 0:
-                return False
+            if cluster_id:
+                if device.get_vendor().get_system_serial() == system_serial and device.get_vendor().get_cluster_id() == 0:
+                    return False
+            else:
+                if device.get_vendor().get_system_serial() == system_serial and device.get_vendor().get_host_id() == 0:
+                    return False
         return True
 
     return all_storage_devices_on_logical_unit_0_of_specific_box_show_non_zero_host_id
