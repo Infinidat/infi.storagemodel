180f66b6995591fa5ba4319f28b8258ee50fe8d9
Amir Reshef <amirr@infinidat.com>
2016-04-11 17:51:51 +0300

    STORAGEMODEL-365 added rescan in iscsi

---
+++ src/infi/storagemodel/linux/iscsi.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+from infi.execute import execute_assert_success, execute
+
+logger = getLogger(__name__)
+
+
+def is_iscsiadm_installed():
+    from infi.os_info import get_platform_string
+    if 'centos' or 'redhat' in get_platform_string():
+        logger.debug("checking if iSCSI sw is installed")
+        process = execute_assert_success(['rpm', '-qa', '--quiet', 'iscsi-initiator-utils'])
+        if process.get_returncode() != 0:
+            logger.debug("iscsi sw isn't installed")
+            return
+        else:
+            logger.debug("iscsi sw installed")
+            return True
+
+
+def iscsi_rescan():
+    if is_iscsiadm_installed():
+        execute_assert_success(['iscsiadm', '-m', 'session', '--rescan'])
