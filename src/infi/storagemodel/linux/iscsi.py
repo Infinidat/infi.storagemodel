21512252138eb24a84c0e43627ec5172c61900fb
Arnon Yaari <arnony@infinidat.com>
2016-04-13 18:06:38 +0300

    fix iscsi rescan

--- src/infi/storagemodel/linux/iscsi.py
+++ src/infi/storagemodel/linux/iscsi.py
@@ -1,22 +1,23 @@
 from logging import getLogger
-from infi.execute import execute_assert_success, execute
+from infi.execute import execute
 
 logger = getLogger(__name__)
 
 
 def is_iscsiadm_installed():
     from infi.os_info import get_platform_string
-    if 'centos' or 'redhat' in get_platform_string():
+    if 'centos' in get_platform_string() or 'redhat' in get_platform_string():
         logger.debug("checking if iSCSI sw is installed")
-        process = execute_assert_success(['rpm', '-qa', '--quiet', 'iscsi-initiator-utils'])
+        process = execute(['/bin/rpm', '-q', '--quiet', 'iscsi-initiator-utils'])
         if process.get_returncode() != 0:
             logger.debug("iscsi sw isn't installed")
-            return
+            return False
         else:
             logger.debug("iscsi sw installed")
             return True
+    return False
 
 
 def iscsi_rescan():
     if is_iscsiadm_installed():
-        execute(['iscsiadm', '-m', 'session', '--rescan'])
+        execute(['/sbin/iscsiadm', '-m', 'session', '--rescan'])
