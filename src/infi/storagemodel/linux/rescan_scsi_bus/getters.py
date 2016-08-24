b867d2e1ddc2444da61b6a0f41ca9a2c9e7060e4
Guy Rozendorn <guy@rzn.co.il>
2016-08-24 11:03:37 +0300

    HOSTDEV-2432 revert

--- src/infi/storagemodel/linux/rescan_scsi_bus/getters.py
+++ src/infi/storagemodel/linux/rescan_scsi_bus/getters.py
@@ -81,14 +81,9 @@ def is_there_a_bug_in_target_removal():
     # once the FC driver deletes the remote port
     # There is a sublte race in the Ubuntu kernel so we don't remove the devices manually
     distname, _, _ = linux_distribution()
-    # HOSTDEV-2432 we see stack traces in all distros, except for redhat-5
-    if distname.lower().split()[0] in ['red', 'redhat', 'centos'] and version.startswith('5'):
-        return False
-    return True
+    return distname in ["Ubuntu"]
 
 def is_there_a_bug_in_sysfs_async_scanning():
-    # HOSTDEV-2432 we see stack traces in all distros
-    return True
     from platform import linux_distribution
     # http://lkml.indiana.edu/hypermail/linux/kernel/0704.2/1108.html
     distname, version, _ = linux_distribution()
