77d35924ec90980d531a96f065cbf52f64621c63
Amit Oren <aoren@infinidat.com>
2018-03-07 17:17:29 +0200

    HPT-2238 add get_wwid method for LinuxBlockDeviceMixin

--- src/infi/storagemodel/linux/block.py
+++ src/infi/storagemodel/linux/block.py
@@ -1,6 +1,12 @@
 from infi.pyutils.lazy import cached_method
 
+
 class LinuxBlockDeviceMixin(object):
+    NAA_DESIGNATOR_TYPE = 0x03
+    LOGICAL_UNIT_ASSOCIATION = 0x00
+
+    INQUIRY_IDENTIFICATION_PAGE_NUM = 0x83
+
     @cached_method
     def get_block_access_path(self):
         return "/dev/%s" % self.sysfs_device.get_block_device_name()
@@ -12,3 +18,26 @@ class LinuxBlockDeviceMixin(object):
     @cached_method
     def get_size_in_bytes(self):
         return self.sysfs_device.get_size_in_bytes()
+
+    @cached_method
+    def get_device_identification_page(self):
+        try:
+            device_identification_page = self.get_scsi_inquiry_pages()[self.INQUIRY_IDENTIFICATION_PAGE_NUM]
+            return device_identification_page
+        except KeyError:    # Device does not support inquiry page 0x83
+            return None
+
+    @cached_method
+    def get_wwid(self):
+        import binascii
+        from infi.asi.cdb.inquiry.vpd_pages.designators import NAA_Descriptor
+        device_identification_page = self.get_device_identification_page()
+        if not device_identification_page:
+            return None
+        for designator in device_identification_page.designators_list:
+            if not isinstance(designator, NAA_Descriptor):
+                continue
+            if designator.designator_type == self.NAA_DESIGNATOR_TYPE and \
+               designator.association == self.LOGICAL_UNIT_ASSOCIATION:
+                return '3{}'.format(binascii.hexlify(designator.pack())[8:].decode('ASCII'))
+        return None
